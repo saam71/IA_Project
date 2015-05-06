202666dcec120033c01a883a643418b10110ed8c
eugenio santos <1evsantos8@gmail.com>
2015-05-06 12:35:15 +0100

    stuff

--- src/OptArea/OptAreaIndividual.java
+++ src/OptArea/OptAreaIndividual.java
@@ -17,11 +17,11 @@ public class OptAreaIndividual extends PosVectorIndividual <OptArea>{
     public OptAreaIndividual(OptArea problem, int size, int altura, int largura, double prob1s) {
         super(problem, size, altura, largura, prob1s);
         
-//        System.out.println("genoma:");
-//        for(int p = 0; p< genome.length; p+=3){
-//            System.out.println("x:" + genome[p] + " y:" + genome[p+1] + " r:" + genome[p+2]);
-//        }
-//        System.out.println("\n");
+        System.out.println("genoma:");
+        for(int p = 0; p< genome.length; p+=3){
+            System.out.println("x:" + genome[p] + " y:" + genome[p+1] + " r:" + genome[p+2]);
+        }
+        System.out.println("\n");
     }
 
     public OptAreaIndividual(OptAreaIndividual original) {
