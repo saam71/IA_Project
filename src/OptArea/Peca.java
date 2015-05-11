de2a817d8ab978ce91d12ebb8bf7d44ee9e68e85
saam71 <saam7.1@gmail.com>
2015-05-11 17:41:05 +0100

    - Rotação implementada na class Peça;

--- src/OptArea/Peca.java
+++ src/OptArea/Peca.java
@@ -5,6 +5,8 @@
  */
 package OptArea;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Eugenio
@@ -12,12 +14,34 @@ package OptArea;
 public class Peca {
     private int id;
     private int[][] forma;
+    private int[][] forma90;
+    private int[][] forma180;
+    private int[][] forma270;
     private int altura;
     private int largura;
+    private ArrayList<int[][]> formasTest = new ArrayList<>(3);
 
     public Peca(int id, int[][] forma, int altura, int largura) {
         this.id = id;
         this.forma = forma;
+        this.forma90 = rotMatrix(this.forma);
+        this.forma180 = rotMatrix(this.forma90);
+        this.forma270 = rotMatrix(this.forma180);
+        formasTest.add(this.forma);
+        formasTest.add(this.forma90);
+        formasTest.add(this.forma180);
+        formasTest.add(this.forma270);
+
+        for (int[][] form : formasTest){
+            StringBuilder str = new StringBuilder();
+            for (int i=0; i<form.length; i++){
+                for (int j=0; j<form[0].length; j++){
+                    str.append(form[i][j]);
+                }
+                str.append("\n");
+            }
+            System.out.println(str);
+        }
         this.altura = altura;
         this.largura = largura;
     }
@@ -33,6 +57,40 @@ public class Peca {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int[][] getForma(int rot){
+        switch (rot){
+            case 0:
+                return forma;
+            case 1:
+                return forma90;
+            case 2:
+                return forma180;
+            case 3:
+                return forma270;
+            default:
+                return null;
+        }
+    }
+
+
+/*
+	this method rotates a matrix 90 degrees to the right
+	the largura and altura are the rotated matrix values
+*/
+
+    private int[][] rotMatrix(int[][] original)
+    {
+        int largura = original.length;
+        int altura = original[0].length;
+        int[][] rotated = new int[altura][largura];
+        for (int i = 0; i < altura; ++i) {
+            for (int j = 0; j < largura; ++j) {
+                rotated[i][j] = original[largura - j - 1][i];
+            }
+        }
+        return rotated;
     }
     
     
